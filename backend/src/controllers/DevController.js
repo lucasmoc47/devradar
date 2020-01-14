f4bb99bc9fbf6da6d9fb0132483436711089fa87
Lucas <lucasmoc47@gmail.com>
2020-01-14 13:31:15 -0300

    Avoiding duplicated Devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -6,24 +6,29 @@ module.exports = {
         const { github_username, techs, latitude, longitude } = req.body
         const userData = await axios.get(`https://api.github.com/users/${github_username}`)
         
-        const { name = login, avatar_url, bio } = userData.data
-    
-        const techsArray = techs.split(',').map(tech => tech.trim())
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
+        let dev = await Dev.findOne({ github_username })
+
+        if(!dev){
+            const { name = login, avatar_url, bio } = userData.data
+        
+            const techsArray = techs.split(',').map(tech => tech.trim())
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+        
+            dev = await Dev.create({
+                github_username, 
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            }).catch(e => console.log(e))
+        
         }
-    
-        const dev = await Dev.create({
-            github_username, 
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        }).catch(e => console.log(e))
-    
-        res.json(dev)
+
+        return res.json(dev)
     }
 }
