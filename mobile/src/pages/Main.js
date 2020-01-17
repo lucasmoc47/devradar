d1d87596a0ca408ce8eb6e1e7f42552c5eb3e816
Lucas <lucasmoc47@gmail.com>
2020-01-17 11:54:42 -0300

    Adding search form

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
 
 function Main({ navigation }){
     const [currentRegion, setCurrentRegion] = useState(null)
@@ -34,20 +35,35 @@ function Main({ navigation }){
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -16.7211438, longitude: -43.8579896 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/10157033?s=460&v=4' }}/>
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'lucasmoc47' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Lucas Caribé</Text>
-                        <Text style={styles.devBio}>No Bio</Text>
-                        <Text style={styles.devTechs}>ReactJS, ReactNative, NodeJS</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -16.7211438, longitude: -43.8579896 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/10157033?s=460&v=4' }}/>
+                    
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'lucasmoc47' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Lucas Caribé</Text>
+                            <Text style={styles.devBio}>No Bio</Text>
+                            <Text style={styles.devTechs}>ReactJS, ReactNative, NodeJS</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm} >
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor='#999'
+                    autoCapitalize='words'
+                    autoCorrect='false'
+                />
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name='my-location' size={20} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
@@ -80,6 +96,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: row
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: center,
+        alignItems: center,
+        marginLeft: 15
     }
 })
 
