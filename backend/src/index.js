68f8cbe4e73a8e7525461d699a355c6aef953b6d
Lucas <lucasmoc47@gmail.com>
2020-01-18 11:55:54 -0300

    Setting up socketio

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const http = require('http')
+
 const routes = require('./routes')
+const { setupWebSocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebSocket(server)
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-4t7ay.mongodb.net/omnistack10?retryWrites=true&w=majority', { 
     useNewUrlParser: true, 
@@ -14,4 +20,4 @@ app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
